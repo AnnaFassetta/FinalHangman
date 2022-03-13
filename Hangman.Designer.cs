b5abae8a7f0b2b09f1e04e87ad3e2864a44ee621
Anna <Anna@DESKTOP-JG4Q650>
2022-03-13 16:17:06 +0200

    Add project files.

---
+++ Hangman.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace FinalHangman
+{
+    partial class Hangman
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.typedAnswer = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.lblChecker = new System.Windows.Forms.Label();
+            this.btnNewWord = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblDisplayScore = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // typedAnswer
+            // 
+            this.typedAnswer.Location = new System.Drawing.Point(12, 67);
+            this.typedAnswer.Name = "typedAnswer";
+            this.typedAnswer.Size = new System.Drawing.Size(184, 23);
+            this.typedAnswer.TabIndex = 0;
+            this.typedAnswer.TextChanged += new System.EventHandler(this.typedAnswer_TextChanged);
+            this.typedAnswer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.typedAnswer_KeyPress);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(12, 46);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(102, 15);
+            this.lblAnswer.TabIndex = 1;
+            this.lblAnswer.Text = "Type your guesses";
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Location = new System.Drawing.Point(12, 19);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(34, 15);
+            this.lblWord.TabIndex = 2;
+            this.lblWord.Text = "word";
+            // 
+            // lblChecker
+            // 
+            this.lblChecker.AutoSize = true;
+            this.lblChecker.Location = new System.Drawing.Point(12, 244);
+            this.lblChecker.Name = "lblChecker";
+            this.lblChecker.Size = new System.Drawing.Size(38, 15);
+            this.lblChecker.TabIndex = 3;
+            this.lblChecker.Text = "label1";
+            this.lblChecker.Visible = false;
+            // 
+            // btnNewWord
+            // 
+            this.btnNewWord.Location = new System.Drawing.Point(433, 281);
+            this.btnNewWord.Name = "btnNewWord";
+            this.btnNewWord.Size = new System.Drawing.Size(75, 23);
+            this.btnNewWord.TabIndex = 4;
+            this.btnNewWord.Text = "New word";
+            this.btnNewWord.UseVisualStyleBackColor = true;
+            this.btnNewWord.Click += new System.EventHandler(this.btnNewWord_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(522, 281);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(85, 23);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(433, 34);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(93, 15);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "Correct Answers";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(433, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(330, 133);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "This is the word structure";
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(12, 259);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(38, 15);
+            this.lblLength.TabIndex = 9;
+            this.lblLength.Text = "label1";
+            this.lblLength.Visible = false;
+            // 
+            // lblDisplayScore
+            // 
+            this.lblDisplayScore.AutoSize = true;
+            this.lblDisplayScore.Location = new System.Drawing.Point(433, 181);
+            this.lblDisplayScore.Name = "lblDisplayScore";
+            this.lblDisplayScore.Size = new System.Drawing.Size(38, 15);
+            this.lblDisplayScore.TabIndex = 10;
+            this.lblDisplayScore.Text = "label1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(578, 67);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 94);
+            this.listBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(578, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Incorrect Answers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Score";
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.lblDisplayScore);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnNewWord);
+            this.Controls.Add(this.lblChecker);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.typedAnswer);
+            this.Name = "Hangman";
+            this.Text = "Hangman Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox typedAnswer;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblChecker;
+        private System.Windows.Forms.Button btnNewWord;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblDisplayScore;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hangman.cs
@@ -0,0 +1,257 @@
+﻿//Filename: FinalHangman
+//Author: Anna Fassetta
+//Created: 07/01/2022
+//Operating System: Windows
+//Version: 10
+//Description: Hangman game where user inputs letters to guess a word
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections; // Required for ArrayList
+namespace FinalHangman
+
+
+{
+    // Form Class where user inputs letters
+    //Anna Fassetta
+    //07/01/2022
+    
+    public partial class Hangman : Form
+    {
+        int WrongWordCounter = 0;
+    
+        int Counter = 0;
+        string guesingName = "";
+        string DisplayName = "";
+        ArrayList StoredValues = new ArrayList();
+        ArrayList StoredCorrectValues = new ArrayList();
+        ArrayList StoredIncorrectCorrectValues = new ArrayList();
+        SortedList<int, string> sortedlist = new SortedList<int, string>();
+        List<Label> labels = new List<Label>();
+        public Hangman()
+        {
+            InitializeComponent();
+        }
+
+        private void typedAnswer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // When program starts it will run the this function
+            //Anna Fassetta
+            //07/01/2022
+
+            GenerateNewWord();
+        }
+
+        void GenerateNewWord()
+        {
+            // Method to create a new random word as well as clears the previous values and rest game.
+            lblDisplayScore.Visible = false;
+            labels = new List<Label>();
+            ArrayList al = new ArrayList();
+            al.Add("hurry"); //5
+            al.Add("door");//4
+            al.Add("office");//6
+         //   al.Add("bat");//3
+            al.Add("instructions");//12
+            al.Add("hospital");//8
+            al.Add("academy");//7
+            al.Add("necessary");//9
+            al.Add("strawberry");//10
+
+            var random = new Random();
+            int index = random.Next(al.Count);
+            guesingName = al[index].ToString();
+
+            char[]chars = guesingName.ToCharArray();
+            lblLength.Text = chars.Length.ToString();
+            int between = 330 / chars.Length;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                labels.Add(new Label());
+                labels[i].Location =  new Point((i * between) + 10,80); //Setting up the display of text
+                labels[i].Text = "_";
+                labels[i].Parent = groupBox1;
+                labels[i].BringToFront();
+                labels[i].CreateControl();
+            }
+
+
+            lblChecker.Text = guesingName;
+            Counter = 0;
+            WrongWordCounter = 0;
+            lblAnswer.Visible = true;
+            lblChecker.Visible = false;
+            lblWord.Visible = true;
+            typedAnswer.Text = "";
+            lblWord.Text = "";
+            DisplayName = "";
+        }
+        private void typedAnswer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // This is the key press function 
+            //Anna Fassetta
+            //07/01/2022
+            Counter = Counter + 1;
+            char OrignalWord = e.KeyChar;
+
+            if (!CheckForLettersOnly(e.KeyChar.ToString()))
+            {
+                MessageBox.Show("Only Letters");
+                return;
+            }
+
+            if (WrongWordCounter > 9)
+            {
+                //Stop Program after 10 attempts and gets a new word
+
+                StoredIncorrectCorrectValues.Add(guesingName);//Store Incorrect values in a list
+                MessageBox.Show("Answer is wrong and you have reached your attempts!");
+                lblAnswer.Visible = false;
+                lblChecker.Visible = false;
+                lblWord.Visible = false;
+
+
+                
+
+                int GetTotalTries = StoredCorrectValues.Count + StoredIncorrectCorrectValues.Count;
+                listBox2.DataSource = null;
+                listBox2.Items.Clear();
+                listBox2.DataSource = StoredIncorrectCorrectValues;
+                label2.Text = "Score " + StoredCorrectValues.Count + " / " + GetTotalTries;
+                GenerateNewWord();
+            }
+            else
+            {
+           
+
+
+
+                if (guesingName.Contains(OrignalWord)) //If the letter contains the word
+                {
+
+                 
+                    char[] letters = guesingName.ToCharArray();
+
+                    for (int i = 0; i < letters.Length; i++)
+                    {
+                        if (letters[i] == OrignalWord)
+                        {
+                            labels[i].Text = OrignalWord.ToString();                        
+                        }                    
+                    }
+              
+                    foreach (Label eachword in labels)
+                      if (eachword.Text == "_") ;
+
+
+
+                    string MainWord = "";
+                    foreach (Label eachword in labels)
+                    {
+                        MainWord += eachword.Text;
+                    }
+
+                    if (MainWord == guesingName) // if generated word matches the guessingName label then the user wins and we store the word into an array list. A new word then gets generated.
+                    {
+
+                        StoredCorrectValues.Add(guesingName);//Store correct values in a list
+
+                        listBox1.DataSource = null;
+                        listBox1.Items.Clear();
+                        listBox1.DataSource = StoredCorrectValues;
+                        MessageBox.Show("Won");
+                        GenerateNewWord();
+
+                        int GetTotal = StoredCorrectValues.Count + StoredIncorrectCorrectValues.Count;
+                        label2.Text = "Score " + StoredCorrectValues.Count + " / " + GetTotal;
+                        return;
+                    }
+                    else
+                    {
+                      
+                    }
+
+                    //Word Correct add to sortedList
+                    try
+                    {
+                        sortedlist.Add(Counter, guesingName);
+                    }
+                    catch (Exception ex)
+                    {
+                        //
+                    }
+
+
+                    //StoredCorrectValues.Add(guesingName);//Store correct values in a list
+                    //MessageBox.Show("Won");
+                }
+                else
+                {
+                    //Letter does not match any of the word
+                    WrongWordCounter = WrongWordCounter + 1;
+                   
+       
+                    MessageBox.Show("Try again");
+                }
+
+
+
+
+                listBox1.DataSource = null;
+                listBox1.Items.Clear();
+                listBox1.DataSource = StoredCorrectValues;
+
+                listBox2.DataSource = null;
+                listBox2.Items.Clear();
+                listBox2.DataSource = StoredIncorrectCorrectValues;
+                lblWord.Text = DisplayName + WrongWordCounter;
+                int GetTotalTries = StoredCorrectValues.Count + StoredIncorrectCorrectValues.Count;
+                label2.Text = "Score " + StoredCorrectValues.Count + " / " + GetTotalTries;
+            }
+        }
+
+        public static bool CheckForLettersOnly(string s) // Method ensures no numbers are inserted
+        {
+            foreach (char c in s)
+            {
+                if (!Char.IsLetter(c))
+                    return false;
+            }
+            return true;
+        }
+
+        private void btnNewWord_Click(object sender, EventArgs e)
+        {
+            GenerateNewWord();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure?", "Close Application", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                MessageBox.Show("Thank you!", "Application Closed!", MessageBoxButtons.OK);
+                System.Windows.Forms.Application.Exit();
+            }
+            else
+            {
+                this.Activate();
+            }
+        }
+
+
+    }
+}
